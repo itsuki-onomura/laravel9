2be6d0f8b50e912d8ab4a8f191175374d1887319
onomura <itsuki100429@gmail.com>
2021-12-15 10:04:32 +0000

    laravel14

---
+++ database/migrations/2021_12_15_094531_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+             $table->string('name'); // ニュースのタイトルを保存するカラム
+             $table->string('gender');// ニュースの本文を保存するカラム
+             $table->string('hobyy');
+             $table->string('introduction');
+            $table->string('image_path')->nullable();  // 画像のパスを保存するカラム
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
